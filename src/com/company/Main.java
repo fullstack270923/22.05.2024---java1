b74c63c76a197ee478b9c199a06e37e09c5cb958
fullstack270923 <148704300+fullstack270923@users.noreply.github.com>
2024-05-22 21:34:41 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    /*
+    function main(args) {
+        console.log('Hello Java!');
+    }
+    const main = (args) => {
+        console.log('Hello Java!');
+    }
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Danny!");
+        float x = 1.5f;
+        int x_int = (int)x; // 1
+        int y;
+        y = 1;
+        System.out.println(y);
+        float z = y; // 1.0
+    }
+}
